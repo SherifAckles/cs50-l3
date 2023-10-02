1228163f712d6e7e43a4bf9e7240a89c396b7087
nabesteron <49988463+nabesteron@users.noreply.github.com>
2023-10-02 17:20:05 -0500

    created http req and res

--- hello/urls.py
+++ hello/urls.py
@@ -3,5 +3,8 @@ from . import views
 #define a var as a list representing all urls can be accessed with this app
 
 urlpatterns=[
-    path("",views.index,name="index")
+    path("",views.index,name="index"),
+    path("<str:name>",views.greet,name="greet"),
+    path("sherif",views.sherif,name="sherif"),
+    path("david",views.david, name="david"),
 ]
